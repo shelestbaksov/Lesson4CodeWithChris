ce2933470e024931616f03d3cf382bcd748e0021
Leisan Latypova <latley90@gmail.com>
2023-01-24 11:36:51 +0300

    Initial Commit

---
+++ Lesson4CodeWithChris/Lesson4CodeWithChrisApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lesson4CodeWithChrisApp.swift
+//  Lesson4CodeWithChris
+//
+//  Created by Leysan Latypova on 24.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct Lesson4CodeWithChrisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
